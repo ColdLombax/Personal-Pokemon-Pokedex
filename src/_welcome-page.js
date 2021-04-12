505e0bc31bd0d53eeee2422299a777a30ba1654d
John Phillips <coldlombax@gmail.com>
2021-04-12 15:37:25 +0100

    Custom username now works with localstorage.

--- src/_welcome-page.js
+++ src/_welcome-page.js
@@ -10,11 +10,32 @@ const content = document.querySelector('.content');
     content.appendChild(pokeballIcon);
 
     const welcomeText = newElement('p', 'welcome-text');
-    welcomeText.textContent = 'Welcome';
+    welcomeText.textContent = 'WELCOME BACK';
     content.appendChild(welcomeText);
 
     // FORM DATA FOR NAME, USING LOCAL STORAGE TO IF THE NAME
+    if (!localStorage.getItem('name')) {
+        // USER HAS NO NAME
+        welcomeText.textContent = 'WELCOME';
 
+        const form = document.createElement('form');
+        form.id = 'first-time';
+
+        const input = newElement('input', 'form-name');
+        input.id = 'name';
+        input.type = 'text';
+        input.placeholder = 'NAME';
+
+        form.appendChild(input);
+        content.appendChild(form);
+    } else {
+        const userName = newElement('p', 'user-text');
+        userName.textContent = `${localStorage.getItem('name')}`
+        content.appendChild(userName);
+    }
+    const launchText = newElement('p', 'launch-button');
+    launchText.textContent = 'LAUNCH';
+    content.appendChild(launchText);
 })();
 
 // CHECKS FOR EVENT LISTNER
@@ -24,6 +45,27 @@ const newPageBtn = (() => {
 })();
 
 function launchButton () {
+    // STARTING ANIMATION IF NAME IN LOCAL STORAGE
+    if (localStorage.getItem('name')) {
+        navbarAnimation();
+
+        // REMOVE ELEMENTS WITH DELAY
+        setTimeout(() => {
+            content.clear();
+        }, 600);
+    }
+    // GETTING NAME DATA AND STORING TO LOCALSTORAGE
+    if (document.querySelector('#name') != null) {
+        const name = document.querySelector('#name').value;
+        localStorage.setItem('name', `${name}`);
+
+        if (!document.querySelector('#name').value) {
+            alert('Please enter a name');
+        }
+    }
+}
+
+function navbarAnimation() {
     // START NAVBAR ANIMATION
     (() => {
         const homeBottomBar = document.querySelector('.nav-bar');
@@ -33,9 +75,4 @@ function launchButton () {
             homeBottomBar.style.height = '40px';
         }, 1000);
     })();
-
-    // REMOVE ELEMENTS WITH DELAY
-    setTimeout(() => {
-        content.clear();
-    }, 600);
 }
